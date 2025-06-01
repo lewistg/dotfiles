c24d09b904994d544ec5721814833f6aea77042f
Ty Lewis <tyglewis@gmail.com>
2025-05-31 20:13:39 -0600

    Fix grep.lua formatting

--- .config/nvim/plugin/grep.lua
+++ .config/nvim/plugin/grep.lua
@@ -1,29 +1,22 @@
 local function grep_visually_selected_text()
-    local _, start_line, start_col = unpack(vim.fn.getpos("v"))
-    local _, end_line, end_col = unpack(vim.fn.getpos("."))
-    if (start_line ~= end_line) then
-        return
-    end
+	local _, start_line, start_col = unpack(vim.fn.getpos("v"))
+	local _, end_line, end_col = unpack(vim.fn.getpos("."))
+	if start_line ~= end_line then
+		return
+	end
 
-    local selected_text = vim.api.nvim_buf_get_text(
-        0,
-        start_line - 1,
-        start_col - 1,
-        end_line - 1,
-        end_col,
-        {}
-    )
+	local selected_text = vim.api.nvim_buf_get_text(0, start_line - 1, start_col - 1, end_line - 1, end_col, {})
 
-    -- Exit visual mode
-    local escape_key = vim.api.nvim_replace_termcodes("<Esc>", true, false, true)
-    vim.api.nvim_feedkeys(escape_key, "nx", false)
+	-- Exit visual mode
+	local escape_key = vim.api.nvim_replace_termcodes("<Esc>", true, false, true)
+	vim.api.nvim_feedkeys(escape_key, "nx", false)
 
-    vim.cmd.grep {
-        args = selected_text,
-        bang = true,
-    }
+	vim.cmd.grep({
+		args = selected_text,
+		bang = true,
+	})
 
-    vim.cmd.copen()
+	vim.cmd.copen()
 end
 
-vim.keymap.set('v', '<Leader>g', grep_visually_selected_text)
+vim.keymap.set("v", "<Leader>g", grep_visually_selected_text)
